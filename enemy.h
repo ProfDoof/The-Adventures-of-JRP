e3b4e04c34b1865fe37822d8dfaba05854daed41
John M <johnswork15@gmail.com>
2017-11-02 14:21:01 -0500

    Edits 1.0.0
    Fleshing out the enemy header file and adding things to the cpp.

--- enemy.h
+++ enemy.h
@@ -2,7 +2,39 @@
 class Enemy: public Character
 {
 	private:
-
+		//private constructor in the base and derived classes
+		Enemy();
 	public:
+		//a static method of a class that returns an object of that class type.
+		static Enemy* createEnemy(int type);
+};
 
-}
+class Guard: public Enemy
+{
+
+};
+
+class Skeleton: public Enemy
+{
+
+};
+
+class Slime: public Enemy
+{
+
+};
+
+class Slaver: public Enemy
+{
+
+};
+
+class Zombie: public Enemy
+{
+
+};
+
+class Wolf: public Enemy
+{
+
+};
