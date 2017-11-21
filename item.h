b3a8f649d283604560cced8261d952ab554ad91c
John M <johnswork15@gmail.com>
2017-11-21 13:19:02 -0600

    Adding Item
    Adding lowercase item back

---
+++ item.h
@@ -0,0 +1,35 @@
+#ifndef ITEM_H
+#define ITEM_H
+#include <iostream>
+#include <string>
+
+class Character;
+
+class Item {
+	private:
+        std::string _Name;
+        std::string _Description;
+		int _Damage;
+		int _Healing;
+		int _Defense;
+		
+	public:
+		Item(std::string Nam, std::string Desc, int Dam, int Heal, int Def) : _Name(Nam), _Description(Desc), _Damage(Dam), _Healing(Heal), _Defense(Def) {}
+		
+		//Getters and Setters
+		bool operator==(const std::string compare);
+		void Name(std::string N) { _Name = N; }
+        std::string Name() { return _Name; }
+		void Description(std::string D) { _Description = D; }
+        std::string Description() { return _Description; }
+		void Damage(int K) { _Damage = K; }
+		int Damage() const { return _Damage; }
+		void Healing(int H) { _Healing = H; }
+		int Healing() const { return _Healing; }
+		void Defense(int D) { _Defense = D; }
+		int Defense() const { return _Defense; }
+};
+
+
+
+#endif
